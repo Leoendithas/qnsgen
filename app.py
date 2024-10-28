3be49bca5f2582b811b33efb19387103e31b0906
Leoendithas <122543071+Leoendithas@users.noreply.github.com>
2024-10-28 10:21:19 +0800

    remove password confirm

--- app.py
+++ app.py
@@ -88,13 +88,10 @@ def login_or_register():
         st.subheader("Register")
         new_username = st.text_input("New Username", key="register_username")
         new_password = st.text_input("New Password", type="password", key="register_password")
-        password_confirm = st.text_input("Confirm Password", type="password")
 
         # Register a new user
         if st.button("Register"):
-            if password != password_confirm:
-                st.error("Passwords do not match!")
-            elif username_exists(username):
+            if username_exists(username):
                 st.error("Username is already taken. Please choose a different one.")
             else:
                 add_user(username, password)
